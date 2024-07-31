d051a5a3b1e3c84f0f735d697eff530cd5c51686
alex02r <alex.giochi02@gmail.com>
2024-07-31 19:01:29 +0200

    upgrade: creazione HomeHero components

---
+++ src/components/home/HomeHero.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: 'HomeHero'
+}
+</script>
+<template lang="">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-12">
+                <div class="">
+                    <h1>Home Page</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, provident libero. Illum ad aliquam temporibus, atque eligendi repudiandae beatae consectetur, repellendus ipsam rerum voluptatibus accusantium quos hic nam blanditiis maxime.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="">
+    
+</style>
