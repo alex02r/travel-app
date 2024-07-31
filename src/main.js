d4e69a4bd88e449650ab6080c758be7021664ba5
alex02r <alex.giochi02@gmail.com>
2024-07-31 20:46:44 +0200

    upgrade: import popper.js

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import './style.css'
 import { router } from './router.js'
 import App from './App.vue'
 
+// Importa Bootstrap Bundle che include Popper.js
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 createApp(App).use(router).mount('#app')
