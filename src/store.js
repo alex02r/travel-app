78fda1d35c54904a7e8ae59103a84cd69f15dbe6
alex02r <alex.giochi02@gmail.com>
2024-07-31 22:54:43 +0200

    upgrade: aggiunta lista menu

--- src/store.js
+++ src/store.js
@@ -1,5 +1,14 @@
 import { reactive } from 'vue';
 
 export const store = reactive({
-    
+    menuList: [
+        {
+            name: 'home',
+            label: 'Home Page'
+        },
+        {
+            name: 'addTravel',
+            label: 'Aggiungi Viaggio'
+        }
+    ]
 })
