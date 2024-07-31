17184bc1ebf2676041ffe726c9885227eb524e84
alex02r <alex.giochi02@gmail.com>
2024-07-31 20:47:15 +0200

    upgrade: import della componente header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+
 export default {
-  
+  components:{
+    AppHeader
+  }
 }
 </script>
 <template lang="">
+  <AppHeader/>
   <router-view/>
 </template>
 <style lang="scss">
