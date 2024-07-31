5c2e23f41effff8be6f5280b2dc6a4b816ad6597
alex02r <alex.giochi02@gmail.com>
2024-07-31 22:32:17 +0200

    upgrade: creazione pagina per aggiunta di un viaggio

---
+++ src/pages/AppNewTravel.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'AppNewTravel'
+}
+</script>
+<template lang="">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Aggiungi un nuovo viaggio</h1>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="">
+    
+</style>
