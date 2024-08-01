07e46985a69beb514ddad78c3053cd0454bdf747
alex02r <alex.giochi02@gmail.com>
2024-08-01 19:33:42 +0200

    upgrade: creazione nuova pagina aggiunta tappe

---
+++ src/pages/AppNewStage.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'AppNewStage'
+}
+</script>
+<template lang="">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-12">
+
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="">
+    
+</style>
