b954aeb5c042c993048e758fbd1bd79c1bc20663
alex02r <alex.giochi02@gmail.com>
2024-08-01 14:31:32 +0200

    upgrade: componente per la visualizzazione di un viaggio

---
+++ src/components/travel/TravelCard.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'TravelCard',
+    props:{
+        travel: Object
+    },
+    mounted() {
+        console.log(this.travel);
+    },
+}
+</script>
+<template lang="">
+    <div class="card text-bg-light mb-3" style="max-width: 18rem;">
+        <div class="card-header">Viaggio</div>
+        <div class="card-body">
+          <h5 class="card-title">{{ travel.title }}</h5>
+          <p class="card-text">{{ travel.desc }}</p>
+          <p class="card-text"><small class="text-body-secondary">{{ travel.date }}</small></p>
+        </div>
+    </div>
+</template>
+<style lang="">
+    
+</style>
