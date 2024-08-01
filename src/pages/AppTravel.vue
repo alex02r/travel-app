7693283308194812df8c1db3d604bfe93c356bfb
alex02r <alex.giochi02@gmail.com>
2024-08-01 15:17:36 +0200

    upgrade: creazione pagina per visualizzare un viaggio

---
+++ src/pages/AppTravel.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'AppTravel'
+}
+</script>
+<template lang="">
+    <div class="container py-5">
+        <div class="row">
+            
+        </div>
+    </div>
+</template>
+<style lang="">
+    
+</style>
