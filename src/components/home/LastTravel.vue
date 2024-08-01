f859ff361dc5686d7335a0c53dcbb9497cc3076b
alex02r <alex.giochi02@gmail.com>
2024-08-01 14:48:29 +0200

    upgrade: visualizzazione ultimo viaggio

--- src/components/home/LastTravel.vue
+++ src/components/home/LastTravel.vue
@@ -1,12 +1,41 @@
 <script>
+import TravelCard from '../travel/TravelCard.vue';
+import travelCard from '../travel/TravelCard.vue';
 export default {
-    name: 'LastTravel'
+    name: 'LastTravel',
+    components:{
+        TravelCard
+    },
+    data() {
+        return {
+            travel: {}
+        }
+    },
+    mounted() {
+        this.getLastTravel();
+    },
+    methods: {
+        //funzione per asseggnare l'ultimo viaggio inserito
+        getLastTravel(){
+            //recuperiamo l'array dei viaggi
+            const travelsJSON = localStorage.getItem('travels');
+            const travels = travelsJSON ? JSON.parse(travelsJSON) : []; 
+
+            //asseggnamo il valore alla variabile travel
+            this.travel = travels[travels.length - 1];
+        }
+    },
 }
 </script>
 <template lang="">
     <div class="container py-5">
         <div class="row">
-            <div class="col-12"></div>
+            <div class="col-12">
+                <div class="mb-4">
+                    <h2>Ultimo viaggio</h2>
+                </div>
+                <TravelCard :travel="travel"/>
+            </div>
         </div>
     </div>
 </template>
