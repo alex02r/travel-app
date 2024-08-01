05ac941c5f08b65824e0489be36306af355d58a7
alex02r <alex.giochi02@gmail.com>
2024-08-01 14:33:21 +0200

    upgrade: creazione componente per visualizzare l'ultinmo viaggio aggiunto

---
+++ src/components/home/LastTravel.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'LastTravel'
+}
+</script>
+<template lang="">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-12"></div>
+        </div>
+    </div>
+</template>
+<style lang="">
+    
+</style>
