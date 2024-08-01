876609682dabc13b826e0c0ce3aa34379036a80d
alex02r <alex.giochi02@gmail.com>
2024-08-01 17:47:00 +0200

    upgrade: visualizzazione delle tappe

--- src/components/travel/StepTravel.vue
+++ src/components/travel/StepTravel.vue
@@ -1,11 +1,25 @@
 <script>
 export default {
-    name: 'StepTravel'
+    name: 'StepTravel',
+    props:{
+        stages: Array
+    }
 }
 </script>
 <template lang="">
-    <div>
-        
+    <div class="row">
+        <div class="col-12">
+            <div class="d-flex align-items-center gap-4">
+                <h2>Tappe</h2>
+                <a href="" class="btn btn-light"><i class="fas fa-plus"></i></a>
+            </div>
+        </div>
+        <div class="col-12" v-if="stages.length == 0">
+            <h4>Non sono presenti tappe</h4>
+        </div>
+        <div class="col-12" v-for="(stage, index) in stages" :key="index">
+            
+        </div>
     </div>
 </template>
 <style lang="">
