38430970ea5537e1a8034f585dae3b344583cc5f
alex02r <alex.giochi02@gmail.com>
2024-08-01 14:31:58 +0200

    upgrade: aggiunto vfor per visualizzare tutti i viaggi

--- src/pages/AppMyTravels.vue
+++ src/pages/AppMyTravels.vue
@@ -1,12 +1,39 @@
 <script>
+import TravelCard from '../components/travel/TravelCard.vue';
+
 export default {
-    name: 'AppMyTravels'
+    name: 'AppMyTravels',
+    components:{
+        TravelCard
+    },
+    data() {
+        return {
+            travels: [],
+        }
+    },
+    mounted() {
+        this.getTravels();
+    },
+    methods: {
+        //funzione che recupera l'array dei viaggi
+        getTravels(){
+            const travelsJSON = localStorage.getItem('travels');
+            this.travels = travelsJSON ? JSON.parse(travelsJSON) : [];
+        }
+    },
 }
 </script>
 <template lang="">
-    <div class="container">
-        <div class="row">
+    <div class="container py-5">
+        <div class="row row-gap-4">
             <div class="col-12">
+                <h1>Tutti i miei viaggi</h1>
+            </div>
+            <div class="col-12 col-md-4 col-lg-3" v-for="travel in travels" ::key="travel.id">
+                <TravelCard :travel="travel" />
+            </div>
+            <div class="col-12" v-if="travels.length == 0">
+                <h3>Non ci sono viaggi da visualizzare <router-link to="addTravel" class="link-light link-underline-opacity-25 link-underline-opacity-75-hover">aggiungine uno</router-link></h3>
 
             </div>
         </div>
