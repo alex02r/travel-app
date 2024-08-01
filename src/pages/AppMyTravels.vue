b84f4b0f777adc21658b3860a6b7b61fdf64fa8f
alex02r <alex.giochi02@gmail.com>
2024-08-01 14:57:55 +0200

    upgrade: link per aggiungere un nuovo viaggio

--- src/pages/AppMyTravels.vue
+++ src/pages/AppMyTravels.vue
@@ -28,13 +28,13 @@ export default {
         <div class="row row-gap-4">
             <div class="col-12">
                 <h1>Tutti i miei viaggi</h1>
+                <router-link to="addTravel" class="btn btn-light"><i class="fas fa-plus"></i></router-link>
             </div>
             <div class="col-12 col-md-4 col-lg-3" v-for="travel in travels" ::key="travel.id">
                 <TravelCard :travel="travel" />
             </div>
             <div class="col-12" v-if="travels.length == 0">
                 <h3>Non ci sono viaggi da visualizzare <router-link to="addTravel" class="link-light link-underline-opacity-25 link-underline-opacity-75-hover">aggiungine uno</router-link></h3>
-
             </div>
         </div>
     </div>
