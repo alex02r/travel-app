32a152beb8c3e2105fec4ba95eded0de7aae1c75
alex02r <alex.giochi02@gmail.com>
2024-08-01 13:39:01 +0200

    upgrade: creazione pagina "i miei viaggi"

---
+++ src/pages/AppMyTravels.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'AppMyTravels'
+}
+</script>
+<template lang="">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="">
+    
+</style>
