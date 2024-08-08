83c7320c8c954b8210bd5abe790c44b2b44eca14
alex02r <alex.giochi02@gmail.com>
2024-08-08 18:11:21 +0200

    upgrade: creazione file motions, per gestire le varie animazioni

---
+++ src/motions.js
@@ -0,0 +1,13 @@
+export const motionFadeUp = {
+    initial:{
+        opacity: 0,
+        y: 200
+    },
+    enter: {
+        opacity: 1,
+        y: 0,
+        transition:{
+            duration: 500
+        }
+    }
+}
