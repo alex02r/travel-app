10c559d460e23f5c96ceaf85cbe918854dbda6e5
alex02r <alex.giochi02@gmail.com>
2024-08-08 18:10:55 +0200

    upgrade: import di vue motion

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import { router } from './router.js'
+import { MotionPlugin } from '@vueuse/motion'
 import App from './App.vue'
 
 // Importa Bootstrap Bundle che include Popper.js
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+app.use(router);
+app.use(MotionPlugin);
+
+app.mount('#app')
