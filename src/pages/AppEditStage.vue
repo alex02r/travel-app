08b78666c081f691e0991c38a1838a397c7d9b8a
alex02r <alex.giochi02@gmail.com>
2024-08-09 15:58:36 +0200

    upgrade: creazione pagina per la modifica dello stato di un viaggio

---
+++ src/pages/AppEditStage.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'AppEditStage',
+}
+</script>
+<template lang="">
+    <div class="contaienr">
+        <div class="row">
+            <div class="col-12">
+                
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="">
+    
+</style>
