00748ab2ee641c040cd34944614093776639f24a
alex02r <alex.giochi02@gmail.com>
2024-08-12 17:32:06 +0200

    upgrade: aggiunta delle descrizioni nella pagina info

--- src/pages/AppInfo.vue
+++ src/pages/AppInfo.vue
@@ -4,10 +4,127 @@ export default {
 }
 </script>
 <template lang="">
-    <div class="container">
-        <div class="row">
+    <div class="container py-5">
+        <div class="row row-gap-5 justify-content-center">
+            <!-- Travel-app -->
+            <div class="col-12 col-md-8">
+                <h1>Come funziona Travel App</h1>
+                <p>
+                    Travel App è il tuo compagno di viaggio ideale, progettato per aiutarti a pianificare, gestire e ricordare ogni tappa dei tuoi viaggi. Con la nostra app, puoi creare itinerari dettagliati, tracciare le tue tappe su una mappa interattiva e tenere traccia dei luoghi che hai visitato. Sia che tu stia pianificando una vacanza rilassante o un'avventura on the road, Travel App ti assiste in ogni fase del viaggio.
+                </p>
+            </div>
+            <!-- Aggiunta di un viaggio -->
+            <div class="col-12 col-md-8">
+                <h2>Creazione di un Viaggio</h2>
+                <p>
+                    Creare un nuovo viaggio su Travel App è semplice e intuitivo. Segui questi passi per iniziare a organizzare la tua prossima avventura:
+                    <ol>
+                        <li>
+                            <strong>Accedi alla Sezione "Aggiungi Viaggio":</strong> Clicca sul pulsante "Crea Nuovo Viaggio" nella homepage o nel menu di navigazione.
+                        </li>
+                        <li>
+                            <strong>Inserisci i Dettagli del Viaggio:</strong> Aggiungi il titolo del viaggio, una breve descrizione e la data di inizio del tuo viaggio.
+                        </li>
+                        <li>
+                            <strong>Salva e Inizia ad Aggiungere Tappe:</strong> Una volta completati i dettagli, salva il viaggio. (dovresti trovarti già nella sezione dei dettagli, altrimenti recati cliccando sul riquadro del tuo viaggio nella sezione "I miei Viaggi") da qui inizia ad aggiungere le tappe che vuoi visitare o che hai visitato.
+                        </li>
+                    </ol>
+                </p>
+                <div class="row">
+                    <div class="col-6">
+                        IMG
+                    </div>
+                    <div class="col-6">
+                        IMG
+                    </div>
+                </div>
+            </div>
+            <!-- Gestione Tappe -->
+            <div class="col-12 col-md-8">
+                <div>
+                    <h2>Aggiungere e Gestire le Tappe</h2>
+                    <p>
+                        Ogni viaggio è composto da tappe, e Travel App rende facile aggiungere e organizzare le tue destinazioni:
+                    </p>
+                </div>
+                <div>
+                    <h4>Aggiunta di una Tappa</h4>
+                    <p>
+                        Clicca sul pulsante "+" affianco a "Tappe", nella pagina del viaggio. Inserisci il nome della destinazione, una breve descrizione, la data prevista per la visita, e, se vuoi, una foto rappresentativa, Altrimenti ne viene inserita una di default.
+                    </p>
+                    <div class="row">
+                        <div class="col-6">
+                            IMG
+                        </div>
+                        <div class="col-6">
+                            IMG
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h4>Organizza le Tappe</h4>
+                    <p>
+                        Puoi visualizzare tutte le tappe del tuo viaggio in un pratico stepper, che ti permette di vedere a colpo d'occhio quali tappe hai già completato e quali sono ancora da visitare
+                    </p>
+                </div>
+                <div>
+                    <h4>Segna una Tappa come Completata</h4>
+                    <p>
+                        Una volta visitata una tappa, contrassegnala come completata. Per farlo è necessario cliccare sul nome della tappa, comparirà una modale di conferma, cliccando su "salva" imposterai la Tappa. Verrà visualizzata con un'icona a forma di check e sarà colorata per indicare che è stata completata. Le tappe non completate saranno visualizzate con una "X" e con una colorazione più leggera.
+                    </p>
+                    <div class="row">
+                        <div class="col-6">
+                            IMG
+                        </div>
+                        <div class="col-6">
+                            IMG
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h4>Modifica o Elimina Tappe</h4>
+                    <p>
+                        Se hai bisogno di aggiornare una tappa o rimuoverla, basta cliccare sull'icona <i class=" fb-4 fa-solid fa-ellipsis-vertical"></i> (situata di fianco al nome della tappa) per fa apparire le opzioni modifica e elimina.
+                    </p>
+                    <div class="row">
+                        <div class="col-6">
+                            IMG
+                        </div>
+                        <div class="col-6">
+                            IMG
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Visualizzazione Mappa -->
+            <div class="col-12 col-md-8">
+                <h2>Visualizza le Tappe sulla MAppa</h2>
+                <p>
+                    Uno dei punti di forza di Travel App è la possibilità di visualizzare tutte le tappe del tuo viaggio su una mappa interattiva. Ecco come funziona:
+                    <ol>
+                        <li>
+                            <strong>Mappa Integrata:</strong> Nella pagina del viaggio, tutte le tue tappe sono automaticamente posizionate su una mappa. Ogni tappa è rappresentata da un marker, e puoi cliccare su di esso per visualizzare i dettagli della tappa.
+                        </li>
+                        <li>
+                            <strong>Interazione con la Mappa:</strong> Puoi ingrandire, ridurre e spostarti sulla mappa per esplorare le tue destinazioni. Cliccando su una tappa, potrai visualizzare le informazioni associate, come descrizioni, foto e note.
+                        </li>
+                        <li>
+                            <strong>Navigazione Facile:</strong> La mappa ti aiuta a visualizzare il percorso del tuo viaggio, permettendoti di avere una visione d'insieme del tragitto e delle distanze tra le tappe.
+                        </li>
+                    </ol>
+                </p>
+                <div class="row">
+                    <div class="col-6">
+                        IMG
+                    </div>
+                    <div class="col-6">
+                        IMG
+                    </div>
+                </div>
+            </div>
+            <!-- Funzionalità Offline -->
             <div class="col-12">
-                <h1>Info</h1>
+            
             </div>
         </div>
     </div>
