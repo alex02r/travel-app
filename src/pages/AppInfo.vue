1c785b4e33ad4890834d29b5b6d2d43a5b5fde71
alex02r <alex.giochi02@gmail.com>
2024-08-12 14:32:41 +0200

    upgrade: creazione pagina info

---
+++ src/pages/AppInfo.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'AppInfo'
+}
+</script>
+<template lang="">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Info</h1>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="">
+    
+</style>
