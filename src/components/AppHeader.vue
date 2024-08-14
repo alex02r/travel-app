2ff829776ad1fd00fcf159cbf0a03d05a8c4b616
alex02r <alex.giochi02@gmail.com>
2024-08-15 00:29:16 +0200

    fix: aggiunto link alla home su travel-app

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -19,13 +19,21 @@ export default {
         <div class="container py-3">
             <div class="row">
                 <div class="col-4">
-                    <h2 class="d-none d-md-inline">Travel App</h2>
+                    <h2 class="d-none d-md-inline">
+                        <router-link :to="{ name: 'home' }" class="link-dark link-underline-opacity-0">
+                            Travel App
+                        </router-link>
+                    </h2>
                     <div class="d-md-none">
                         <SideMenu/>
                     </div>
                 </div>
                 <div class="col-8">
-                    <h2 class="text-end d-md-none">Travel App</h2>
+                    <h2 class="text-end d-md-none">
+                        <router-link :to="{ name: 'home' }" class="link-dark link-underline-opacity-0">
+                            Travel App
+                        </router-link>
+                    </h2>
 
                     <ul class="list-unstyled d-none d-md-flex justify-content-end align-items-center gap-4">
                         <li v-for="(item, index) in store.menuList" ::key="index" >
