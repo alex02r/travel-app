cd26a7f31fcadd00476493b71d5dbcabe4e3aab8
alex02r <alex.giochi02@gmail.com>
2024-08-20 14:45:30 +0200

    upgrade: aggiunta la modale e la funzione per eliminare un viaggio

--- src/components/travel/TravelCard.vue
+++ src/components/travel/TravelCard.vue
@@ -1,5 +1,6 @@
 <script>
 import { motionFadeUp } from '../../motions';
+import { StorageService } from '../../localStorage.service';
 
 export default {
     name: 'TravelCard',
@@ -9,6 +10,25 @@ export default {
     data() {
         return {
             motionFadeUp
+        }
+    },
+    methods: {
+        deleteTravel(){
+            //recuperiamo l'array dei viaggi
+            let travels = StorageService.getTravels();
+            //cicliamo l'array
+            for (let i = 0; i < travels.length; i++) {
+                //controlliamo quando visualizziamo il nostro viaggio e lo eliminiamo
+                if (travels[i].id == this.travel.id ) {
+                    travels.splice(i, 1);
+                    break;
+                }
+            }
+            //aggiorniamo l'array
+            StorageService.setTravels(travels);
+            
+            this.$router.push({ name: 'myTravels' });
+            return;
         }
     },
 }
@@ -29,7 +49,9 @@ export default {
                                     <i class="fa-regular fa-pen-to-square"></i> Modifica
                                 </router-link>
                             </li>
-                            <li><a class="dropdown-item" href="#"><i class="fas fa-trash-can"></i> Elimina</a></li>
+                            <li data-bs-toggle="modal" data-bs-target="#travelModal">
+                                <a class="dropdown-item" href="#"><i class="fas fa-trash-can"></i> Elimina</a>
+                            </li>
                         </ul>
                     </div>
                 </div> 
@@ -41,6 +63,30 @@ export default {
                     <p class="card-text"><small class="text-body-secondary">{{ travel.date }}</small></p>
                 </div>
             </router-link>
+        </div>
+    </div>
+    <!-- modal di eliminazione del viaggio -->
+    <div class="modal fade" id="travelModal" tabindex="-1" aria-labelledby="travelModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content text-dark">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="travelModalLabel">{{ travel.title }}</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body py-4">
+                    <p>
+                        Sei sicuro di voler eliminare questo viaggio ?
+                        <br>
+                        <span class="stage-indicator">
+                            Una volta eliminato, non potrai recuperarlo. perderai anche tutte le tappe.
+                        </span>
+                    </p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary rounded-pill" data-bs-dismiss="modal">Annulla</button>
+                    <button type="button" class="btn btn-danger rounded-pill" @click="deleteTravel()" data-bs-dismiss="modal">Elimina</button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
